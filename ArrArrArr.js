fab6f3962d278a269edf26a4f71974c59b8a7ea6
Edward-Pil <140871461+Edward-Pil@users.noreply.github.com>
2023-08-02 19:25:46 +0500

    Arr

---
+++ ArrArrArr.js
@@ -0,0 +1,22 @@
+var x = [5, 3];
+function explode(x){
+    var c=0;
+    var res=[];
+    if ((typeof x[0]==='number')&&(typeof x[1]==='number')){
+        c= x[0]+x[1];
+        for (var i=1; i<c+1; i++){
+            res.push(x);
+        }
+    }
+    else if ((typeof x[0]==='number')||(typeof x[1]==='number')){
+        if (typeof x[0]==='number'){c= x[0]}
+        if (typeof x[1]==='number'){c= x[1]}
+        for (var i=1; i<c; i++){
+            res.push(x);
+        }
+    }
+    else {return 'Void!'}
+    return res;
+}
+var fn = explode(x);
+console.log (fn);
